7c67c0e1957f661de98b100e2d5aec667dd9784a
erwinsgal <100753450+erwinsgal@users.noreply.github.com>
2022-04-08 13:01:57 +0600

    Add files via upload

---
+++ src/main/java/dar/academy/postapigateway/PostApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package dar.academy.postapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostApiGatewayApplication.class, args);
+    }
+
+}
